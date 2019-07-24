5cf40b244cacd1ea5ae2154151e96c7d608e75eb
T Posch <thomas-poschadel@web.de>
2019-07-24 02:08:28 +0200

    intermediate push

--- src/data_handling.py
+++ src/data_handling.py
@@ -56,3 +56,185 @@ def split_features_labels(frame):
     features = frame.drop(label_cols, axis=1)
     labels = frame[label_cols]
     return features, labels
+
+
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
+#
+# File handling provided by author
+# Modified by TP because was written in python 2
+#
+#######################################################################################################################
+import numpy as np;
+from io import StringIO;
+
+
+
+def parse_header_of_csv(csv_str):
+    # Isolate the headline columns:
+    headline = csv_str[:csv_str.index('\n')];
+    columns = headline.split(',');
+
+    # The first column should be timestamp:
+    assert columns[0] == 'timestamp';
+    # The last column should be label_source:
+    assert columns[-1] == 'label_source';
+
+    # Search for the column of the first label:
+    for (ci, col) in enumerate(columns):
+        if col.startswith('label:'):
+            first_label_ind = ci;
+            break;
+        pass;
+
+    # Feature columns come after timestamp and before the labels:
+    feature_names = columns[1:first_label_ind];
+    # Then come the labels, till the one-before-last column:
+    label_names = columns[first_label_ind:-1];
+    for (li, label) in enumerate(label_names):
+        # In the CSV the label names appear with prefix 'label:', but we don't need it after reading the data:
+        assert label.startswith('label:');
+        label_names[li] = label.replace('label:', '');
+        pass;
+
+    return (feature_names, label_names);
+
+
+def parse_body_of_csv(csv_str, n_features):
+    # Read the entire CSV body into a single numeric matrix:
+    full_table = np.loadtxt(csv_str, delimiter=',', skiprows=1);
+
+    # Timestamp is the primary key for the records (examples):
+    timestamps = full_table[:, 0].astype(int);
+
+    # Read the sensor features:
+    X = full_table[:, 1:(n_features + 1)];
+
+    # Read the binary label values, and the 'missing label' indicators:
+    trinary_labels_mat = full_table[:, (n_features + 1):-1];  # This should have values of either 0., 1. or NaN
+    M = np.isnan(trinary_labels_mat);  # M is the missing label matrix
+    Y = np.where(M, 0, trinary_labels_mat) > 0.;  # Y is the label matrix
+
+    return (X, Y, M, timestamps);
+
+
+'''
+Read the data (precomputed sensor-features and labels) for a user.
+This function assumes the user's data file is present.
+'''
+
+
+def read_user_data(uuid):
+    user_data_file =  os.path.join(data_path, '%s.features_labels.csv' % uuid)
+
+    # Read the entire csv file of the user:
+    with open(user_data_file, 'r') as fid:
+        csv_str = fid.read();
+        pass;
+
+    (feature_names, label_names) = parse_header_of_csv(csv_str);
+    n_features = len(feature_names);
+    # ToDo: Fix this! the first paramter was a binary before, but this could no longer be handles in parse header of csv. THerefore
+    #  here again the file path is given and the file loaded a second time. This might be bad for perfomance
+    (X, Y, M, timestamps) = parse_body_of_csv(user_data_file, n_features);
+
+    return (X, Y, M, timestamps, feature_names, label_names);
+
+
+def get_sensor_names_from_features(feature_names):
+    feat_sensor_names = np.array([None for feat in feature_names]);
+    for (fi,feat) in enumerate(feature_names):
+        if feat.startswith('raw_acc'):
+            feat_sensor_names[fi] = 'Acc';
+            pass;
+        elif feat.startswith('proc_gyro'):
+            feat_sensor_names[fi] = 'Gyro';
+            pass;
+        elif feat.startswith('raw_magnet'):
+            feat_sensor_names[fi] = 'Magnet';
+            pass;
+        elif feat.startswith('watch_acceleration'):
+            feat_sensor_names[fi] = 'WAcc';
+            pass;
+        elif feat.startswith('watch_heading'):
+            feat_sensor_names[fi] = 'Compass';
+            pass;
+        elif feat.startswith('location'):
+            feat_sensor_names[fi] = 'Loc';
+            pass;
+        elif feat.startswith('location_quick_features'):
+            feat_sensor_names[fi] = 'Loc';
+            pass;
+        elif feat.startswith('audio_naive'):
+            feat_sensor_names[fi] = 'Aud';
+            pass;
+        elif feat.startswith('audio_properties'):
+            feat_sensor_names[fi] = 'AP';
+            pass;
+        elif feat.startswith('discrete'):
+            feat_sensor_names[fi] = 'PS';
+            pass;
+        elif feat.startswith('lf_measurements'):
+            feat_sensor_names[fi] = 'LF';
+            pass;
+        else:
+            raise ValueError("!!! Unsupported feature name: %s" % feat);
+
+        pass;
+
+    return feat_sensor_names;
+
+
+def get_label_pretty_name(label):
+    if label == 'FIX_walking':
+        return 'Walking';
+    if label == 'FIX_running':
+        return 'Running';
+    if label == 'LOC_main_workplace':
+        return 'At main workplace';
+    if label == 'OR_indoors':
+        return 'Indoors';
+    if label == 'OR_outside':
+        return 'Outside';
+    if label == 'LOC_home':
+        return 'At home';
+    if label == 'FIX_restaurant':
+        return 'At a restaurant';
+    if label == 'OR_exercise':
+        return 'Exercise';
+    if label == 'LOC_beach':
+        return 'At the beach';
+    if label == 'OR_standing':
+        return 'Standing';
+    if label == 'WATCHING_TV':
+        return 'Watching TV'
+
+    if label.endswith('_'):
+        label = label[:-1] + ')';
+        pass;
+
+    label = label.replace('__', ' (').replace('_', ' ');
+    label = label[0] + label[1:].lower();
+    label = label.replace('i m', 'I\'m');
+    return label;
+
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/keras_in_use.py
@@ -0,0 +1,146 @@
+# USAGE
+# python train.py --dataset dataset --model fashion.model --labelbin mlb.pickle
+
+# set the matplotlib backend so figures can be saved in the background
+import gc
+import matplotlib
+from keras import Sequential, optimizers
+from keras.layers import Conv2D, Activation, MaxPooling2D, Dropout, Flatten, Dense
+
+from data_handling import load_user_data, split_features_labels
+
+matplotlib.use("Agg")
+
+# import the necessary packages
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+import os
+
+# # construct the argument parse and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-d", "--dataset", required=True,
+# 	help="path to input dataset (i.e., directory of images)")
+# ap.add_argument("-m", "--model", required=True,
+# 	help="path to output model")
+# ap.add_argument("-l", "--labelbin", required=True,
+# 	help="path to output label binarizer")
+# ap.add_argument("-p", "--plot", type=str, default="plot.png",
+# 	help="path to output accuracy/loss plot")
+# args = vars(ap.parse_args())
+
+# initialize the number of epochs to train for, initial learning rate,
+# batch size, and image dimensions
+EPOCHS = 75
+INIT_LR = 1e-3
+BS = 32
+IMAGE_DIMS = (96, 96, 3)
+
+# # grab the image paths and randomly shuffle them
+# print("[INFO] loading csv...")
+# imagePaths = sorted(list(paths.list_images(args["dataset"])))
+# random.seed(42)
+# random.shuffle(imagePaths)
+
+# initialize the data and labels
+data = []
+labels = []
+
+# read the user data
+user_data = load_user_data()
+features_df, labels_df = split_features_labels(user_data)
+user_data = None
+features_df.reset_index(inplace=True)
+
+# get uuid column and remove them, the timestamps and the label_source from
+# the labels
+Y = labels_df.values
+index_cols = features_df.columns[[0, 1, 2, -1]]
+features_df.drop(index_cols, axis=1, inplace=True)
+X = features_df.values
+features_df = None
+gc.collect()
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2,
+                                                    random_state=42)
+
+# initialize the model using a sigmoid activation as the final layer
+# in the network so we can perform multi-label classification
+print("[INFO] compiling model...")
+model = Sequential()
+model.add(Dense(64, input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Dense(32))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(51))
+model.add(Activation('sigmoid'))
+
+# initiate RMSprop optimizer
+opt = optimizers.rmsprop(lr=0.0001, decay=1e-6)
+
+# Let's train the model using RMSprop
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+# initialize the optimizer (SGD is sufficient)
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+
+# compile the model using binary cross-entropy rather than
+# categorical cross-entropy -- this may seem counterintuitive for
+# multi-label classification, but keep in mind that the goal here
+# is to treat each output label as an independent Bernoulli
+# distribution
+model.compile(loss="binary_crossentropy", optimizer=opt,
+              metrics=["accuracy"])
+
+# train the network
+print("[INFO] training network...")
+H = model.fit(x_train, y_train,
+              batch_size=32,
+              epochs=100,
+              validation_data=(x_test, y_test),
+              shuffle=True,
+              verbose=1)
+
+# save the model to disk
+print("[INFO] serializing network...")
+model.save("model")
+
+# # save the multi-label binarizer to disk
+# print("[INFO] serializing label binarizer...")
+# f = open(args["labelbin"], "wb")
+# f.write(pickle.dumps(mlb))
+# f.close()
+#
+# # plot the training loss and accuracy
+# plt.style.use("ggplot")
+# plt.figure()
+# N = EPOCHS
+# plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+# plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+# plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+# plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+# plt.title("Training Loss and Accuracy")
+# plt.xlabel("Epoch #")
+# plt.ylabel("Loss/Accuracy")
+# plt.legend(loc="upper left")
+# plt.savefig(args["plot"])
