2b246aad9f091514ea0ae1ac629a9aef31fcef49
Joschi <joschka.strueber@uni-muenster.de>
2019-08-13 21:01:10 +0200

    replace cv with single param set

--- src/user_classification.py
+++ src/user_classification.py
@@ -6,9 +6,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, confusion_matrix
-from sklearn.model_selection import RandomizedSearchCV
 from sklearn.pipeline import Pipeline
-from scipy.stats import uniform, reciprocal, randint
 from joblib import dump, load
 
 from data_handling import load_user_data, split_features_labels
@@ -44,36 +42,15 @@ if __name__ == '__main__':
     X_test = pd.DataFrame(preprocess_pipeline.transform(X_test),
                           columns=feature_names)
 
-    param_dist = {
-        "colsample_bytree": uniform(loc=0.8, scale=0.2),
-        "gamma": uniform(loc=0, scale=1),
-        "learning_rate": reciprocal(0.005, 0.1),
-        "max_depth": [5, 6, 7, 8, 9],
-        "n_estimators": [200],
-        "subsample": uniform(loc=0.8, scale=0.2)
-    }
-    clf = xgb.XGBClassifier(objective="multi:softprob", tree_method="gpu_hist")
-    """
     clf = xgb.XGBClassifier(colsample_bytree=0.9,
                             n_estimators=200,
-                            learning_rate=0.01,
+                            learning_rate=0.04,
                             max_depth=8,
-                            subsample=0.8,
-                            gamma=1,
+                            subsample=0.9,
+                            gamma=0.05,
                             objective="multi:softprob", tree_method="gpu_hist")
-    """
-    random_search = RandomizedSearchCV(clf, param_dist, n_iter=10, cv=3,
-                                       verbose=3, n_jobs=1,
-                                       scoring='f1_weighted')
-    random_search.fit(X_train, y_train)
-    print(random_search.best_estimator_)
-    dump(random_search.best_estimator_, "user_class_xgb.joblib")
 
-    preds = random_search.predict(X_test)
-
-    print(random_search.cv_results_)
-    """
-    clf.fit(X_train, y_train.values.ravel())
+    clf.fit(X_train, y_train)
     xgb.plot_importance(clf, max_num_features=20)
     xgb.plot_tree(clf)
     fig = plt.gcf()
@@ -82,9 +59,8 @@ if __name__ == '__main__':
     fig.savefig("user_class_model.png")
 
     dump(clf, "user_class_xgb.joblib")
-
     preds = clf.predict(X_test)
-    """
+
     score_f1 = f1_score(y_test, preds, average="weighted")
     print("F1 score: ", score_f1)
     print(confusion_matrix(y_test, preds))
