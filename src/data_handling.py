fc3bdc76026e2888c3ec3c40487f4390845e82ae
d_beck26 <d_beck26@uni-muenster.de>
2019-08-22 17:26:52 +0200

    add fancyimpute as method and create some meta information as prints

--- src/data_handling.py
+++ src/data_handling.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import os
+import fancyimpute
+import pickle
+from random import randint
 
 from sklearn.model_selection import GroupShuffleSplit
 
@@ -17,7 +20,7 @@ def load_user_data(path=data_path):
     """
     user_keys = []
     user_frames = [process_user(f, user_keys) for f in list_file_paths(path)]
-    combined_user_frames = pd.concat(user_frames, keys=user_keys)
+    combined_user_frames = pd.concat(user_frames, keys=user_keys,sort=False)
     return combined_user_frames
 
 
@@ -87,10 +90,57 @@ def user_train_test_split(X, y, test_size=0.2, random_state=2):
         y_test = y[tt]
     return X_train, X_test, y_train, y_test
 
+def impute_missing_labels(label_matrix):
+    """
+    Impute missing labels (NaN) by using fancyimpute by Alex Rubinsteyn (github.com/iskandr)
+    :author: Daniel Beckmann
+    :param label_matrix: pandas data frame containing the labels for each data sample
+    :return: imputed label matrix without NaN-values
+    """
+    sparse_label_matrix = label_matrix.values
+    size = np.shape(sparse_label_matrix)[0] * np.shape(sparse_label_matrix)[1]
+    print("Label matrix shape: {}".format(np.shape(sparse_label_matrix)))
+
+
+    ones = np.where(sparse_label_matrix == 1, sparse_label_matrix, 0)
+    print("Non-zero values in label matrix: {}".format(float(np.count_nonzero(ones)) / size))
+
+    # full_label_matrix = fancyimpute.KNN(k=3).fit_transform(sparse_label_matrix)
+
+    #full_label_matrix_normalized = fancyimpute.BiScaler().fit_transform(sparse_label_matrix)
+    # full_label_matrix = fancyimpute.SoftImpute(max_iters=500, min_value=0,max_value=1).fit_transform(sparse_label_matrix)
+    # with open("matrix","wb") as f:
+        # pickle.dump(full_label_matrix,f)
+
+    with open("matrix","rb") as f:
+        full_label_matrix = pickle.load(f)
+    sparse_label_matrix_imputed = fancyimpute.SimpleFill(fill_method="zero").fit_transform(sparse_label_matrix)
+    sparse_ratios = [np.average(sparse_label_matrix_imputed[:,x],0) for x in range(51)]
+
+
+    full_label_matrix = np.where(full_label_matrix < 10e-2, full_label_matrix, 1)
+    full_label_matrix = np.where(full_label_matrix >= 10e-2, full_label_matrix, 0)
+
+    print("")
+    print("Full Label matrix shape: {}".format(np.shape(full_label_matrix)))
+    ones = np.where(full_label_matrix == 1,full_label_matrix, 0)
+    print("Non-zero values in full label matrix: {}".format(float(np.count_nonzero(ones)) / size))
+    full_ratios = [np.average(full_label_matrix[:, x], 0) for x in range(51)]
+
+    print("")
+    print("Factors between label appearance ratio:")
+    print(sparse_ratios)
+    print(full_ratios)
+    print([full_ratios[i]/sparse_ratios[i] for i in range(51)])
+
+
+
 
 if __name__ == "__main__":
-    data = load_some_user_data()
+    data = load_user_data()
     attr, labels = split_features_labels(data)
     attr_values = attr.values
     labels_values = labels.values
-    user_train_test_split(attr_values, labels_values)
+    # user_train_test_split(attr_values, labels_values)
+
+    impute_missing_labels(labels)
