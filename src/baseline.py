9a813c3f26b76240c53b2346c919032c2f5d8d8a
Joschi <joschka.strueber@uni-muenster.de>
2019-09-05 19:40:40 +0200

    replace skl ovr with flex_ovr and skl random forest with xgbrfc

--- src/baseline.py
+++ src/baseline.py
@@ -1,9 +1,9 @@
 import numpy as np
+import xgboost as xgb
 
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.linear_model import LogisticRegression
-from sklearn.multiclass import OneVsRestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import make_scorer
@@ -14,6 +14,7 @@ from scipy.stats import reciprocal
 from data_handling import load_user_data, load_some_user_data, \
     split_features_labels, user_train_test_split
 from metrics import balanced_accuracy_score
+from flex_one_vs_rest_classifier import FlexOneVsRestClassifier
 
 
 if __name__ == "__main__":
@@ -42,10 +43,10 @@ if __name__ == "__main__":
     X_train = preprocess_pipeline.fit_transform(X_train)
     X_test = preprocess_pipeline.transform(X_test)
 
+    """
     label_imputer = SimpleImputer(strategy="constant", fill_value=0.0)
     y_train = label_imputer.fit_transform(y_train)
 
-    """
     C_values = {
         "estimator__C": reciprocal(0.001, 1)
     }
@@ -66,53 +67,46 @@ if __name__ == "__main__":
     y_pred = random_search.predict(X_test)
     y_pred_bias = random_search.predict(X_train)
     """
+    n_estimators = y_train.shape[1]
     log_reg_clf = LogisticRegression(solver="lbfgs",
-                                     C=0.1,
-                                     max_iter=1000,
+                                     C=1,
+                                     max_iter=100,
                                      verbose=0,
-                                     tol=5e-4)
-    log_reg_ovr = OneVsRestClassifier(log_reg_clf, n_jobs=-1)
+                                     tol=5e-3)
+    log_reg_ovr = FlexOneVsRestClassifier(clf=log_reg_clf,
+                                          n_estimators=n_estimators)
 
     log_reg_bal_clf = LogisticRegression(solver="lbfgs",
                                          C=0.1,
-                                         max_iter=1000,
+                                         max_iter=100,
                                          verbose=0,
-                                         tol=5e-4,
+                                         tol=5e-3,
                                          class_weight="balanced")
-    log_reg_bal_ovr = OneVsRestClassifier(log_reg_bal_clf, n_jobs=-1)
+    log_reg_bal_ovr = FlexOneVsRestClassifier(clf=log_reg_bal_clf,
+                                              n_estimators=n_estimators)
 
-    random_forest_clf = RandomForestClassifier(n_estimators=1000,
-                                               max_features="sqrt",
-                                               min_samples_leaf=2,
-                                               min_samples_split=5,
-                                               n_jobs=-1)
-    random_forest_ovr = OneVsRestClassifier(random_forest_clf, n_jobs=-1)
-
-    random_forest_bal_clf = RandomForestClassifier(n_estimators=1000,
-                                                   max_features="sqrt",
-                                                   min_samples_leaf=2,
-                                                   min_samples_split=5,
-                                                   n_jobs=-1,
-                                                   class_weight="balanced_subsample")
-    random_forest_bal_ovr = OneVsRestClassifier(random_forest_bal_clf, n_jobs=-1)
-
+    random_forest_clf = xgb.XGBRFClassifier(max_depth=20,
+                                            n_estimators=100,
+                                            tree_method="gpu_hist",
+                                            objective="binary:logistic")
+    random_forest_ovr = FlexOneVsRestClassifier(clf=random_forest_clf,
+                                                n_estimators=n_estimators)
     classifiers = {log_reg_ovr,
                    log_reg_bal_ovr,
-                   random_forest_ovr,
-                   random_forest_bal_ovr}
+                   random_forest_ovr}
 
     for clf in classifiers:
-        print(clf)
+        print(clf.classifiers[0])
         clf.fit(X_train, y_train)
 
         y_pred = clf.predict(X_test)
         y_pred_bias = clf.predict(X_train)
 
         print("Balanced accuracy: ", balanced_accuracy_score(y_test.T,
-                                                             y_pred.T,
+                                                             y_pred,
                                                              average="macro",
                                                              zero_default=0))
         print("Balanced accuracy bias:", balanced_accuracy_score(y_train.T,
-                                                                 y_pred_bias.T,
+                                                                 y_pred_bias,
                                                                  average="macro",
                                                                  zero_default=0))
