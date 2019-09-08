98a8f117ad58fdf3e2fca99bd690657d5a11045a
Joschi <joschka.strueber@uni-muenster.de>
2019-09-08 14:21:59 +0200

    remove test main from flex ovr, implement get and set_params, bug fix for empty int_params in tune_hyperparams

--- src/flex_one_vs_rest_classifier.py
+++ src/flex_one_vs_rest_classifier.py
@@ -1,25 +1,16 @@
-import xgboost as xgb
 import numpy as np
 import shutil
 import os
-import time
 import gc
 
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import StratifiedKFold, GroupKFold
 from joblib import dump, load
-from math import sqrt
 from copy import deepcopy
 from bayes_opt import BayesianOptimization
 from threading import Thread
 
-from data_handling import load_user_data, load_some_user_data, \
-    split_features_labels, user_train_test_split, convert_to_int
-from metrics import balanced_accuracy_score, single_balanced_accuracy_score
+from data_handling import convert_to_int
 
 
 class FlexOneVsRestClassifier(BaseEstimator, ClassifierMixin):
@@ -128,7 +119,8 @@ class FlexOneVsRestClassifier(BaseEstimator, ClassifierMixin):
         """
         param_dict = {}
         for label in range(len(self.classifiers)):
-            param_dict[label] = self.classifiers[label].get_params(deep=deep)
+            param_dict[str(label)] = self.classifiers[label].get_params(
+                deep=deep)
         return param_dict
 
     def set_params(self, **params):
@@ -137,7 +129,8 @@ class FlexOneVsRestClassifier(BaseEstimator, ClassifierMixin):
         :param params:
         :return:
         """
-        return
+        for label in params:
+            self.classifiers[int(label)].set_params(**params[label])
 
     def score(self, X, y, sample_weight=None):
         """
@@ -166,10 +159,9 @@ class FlexOneVsRestClassifier(BaseEstimator, ClassifierMixin):
         return float(count_correct) / n_pred
 
     def tune_hyperparams(self, X, y, bounds, metric, init_points=10, n_iter=20,
-                         int_params=None):
+                         int_params=None, groups=None):
         """
         todo: implement and comment
-        :param int_params:
         :author: Joschka Strüber
         :param X:
         :param y:
@@ -177,6 +169,8 @@ class FlexOneVsRestClassifier(BaseEstimator, ClassifierMixin):
         :param metric:
         :param n_iter:
         :param init_points:
+        :param int_params:
+        :param groups:
         :return:
         """
         for i in range(len(self.classifiers)):
@@ -184,25 +178,32 @@ class FlexOneVsRestClassifier(BaseEstimator, ClassifierMixin):
             label_is_nan = np.isnan(y_i)
             X_i = X[~label_is_nan]
             y_i = y_i[~label_is_nan]
+            groups_no_nan = groups[~label_is_nan]
             # check for empty target
             if y_i.size == 0:
                 continue
             bayes_opt = BayesianOptimization(self.get_evaluate(i, X_i, y_i,
                                                                metric,
-                                                               int_params),
+                                                               int_params,
+                                                               groups_no_nan),
                                              pbounds=bounds)
             bayes_opt.maximize(init_points=init_points, n_iter=n_iter)
-            params = convert_to_int(bayes_opt.max['params'], int_params)
+            if int_params is not None:
+                params = convert_to_int(bayes_opt.max['params'], int_params)
             self.classifiers[i].set_params(**params)
 
-    def get_evaluate(self, label_index, X, y, metric, int_params):
+    def get_evaluate(self, label_index, X, y, metric, int_params, groups):
         def evaluate(**kwargs):
-            kwargs = convert_to_int(kwargs, int_params)
+            if int_params is not None:
+                kwargs = convert_to_int(kwargs, int_params)
             clf = deepcopy(self.classifiers[label_index])
-            strat_kf = StratifiedKFold(n_splits=3, shuffle=True)
+            if groups is None:
+                kfold = StratifiedKFold(n_splits=3)
+            else:
+                kfold = GroupKFold(n_splits=3)
             clf.set_params(**kwargs)
             sum_scores = 0.0
-            for train_index, test_index in strat_kf.split(X, y):
+            for train_index, test_index in kfold.split(X, y, groups=groups):
                 X_tr, X_te = X[train_index], X[test_index]
                 y_tr, y_te = y[train_index], y[test_index]
 
@@ -250,118 +251,3 @@ class FlexOneVsRestClassifier(BaseEstimator, ClassifierMixin):
             shutil.rmtree("tmp")
         gc.collect()
         return clf
-
-
-if __name__ == "__main__":
-    start = time.time()
-    # load data and reset index
-    data = load_user_data()
-    data.reset_index(inplace=True)
-    X, y = split_features_labels(data)
-    attrs = list(X.index)
-    labels = list(y.index)
-    X = X.values
-    y = y.values
-
-    # save save uuid for stratified train-test-split
-    le = LabelEncoder()
-    le.fit(X[:, 0])
-    strat_classes = le.transform(X[:, 0])
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=0.2,
-                                                        random_state=None,
-                                                        stratify=strat_classes)
-    """
-    X_train, X_test, y_train, y_test = user_train_test_split(X, y,
-                                                             test_size=0.2,
-                                                             random_state=None)
-    """
-    # drop uuid column, the timestamps, and the label source
-    X_train = np.delete(X_train, [0, 1, 2, X_train.shape[1] - 1], 1)
-    X_test = np.delete(X_test, [0, 1, 2, X_test.shape[1] - 1], 1)
-
-    clf = xgb.XGBClassifier(objective="binary:logistic",
-                            n_jobs=-1,
-                            tree_method="gpu_hist",
-                            n_estimators=400,
-                            learning_rate=0.02,
-                            max_depth=7,
-                            colsample_bytree=0.8,
-                            gamma=1,
-                            subsample=0.8)
-    """
-    clf = LogisticRegression(solver="lbfgs",
-                             C=1,
-                             max_iter=1000,
-                             n_jobs=-1,
-                             tol=5e-3)
-    """
-    clf = FlexOneVsRestClassifier(clf, n_estimators=y_train.shape[1])
-    """
-    # todo: remove test for perfect scale_pos_weight
-    for label in range(y_train.shape[1]):
-        sum_pos = np.count_nonzero(y_train[:, label] == 1.0)
-        sum_neg = np.count_nonzero(y_train[:, label] == 0)
-        neg_pos_ratio_train = float(sum_neg) / sum_pos if sum_pos != 0 else 1
-        
-        class_weights = {0: 1,
-                         1: neg_pos_ratio_train}
-        params = {"class_weights": class_weights}
-        clf.classifiers[label].set_params(**params)
-        
-        # clf.classifiers[label].scale_pos_weight = sqrt(neg_pos_ratio_train)
-        #print(label, sum_pos + sum_neg, sqrt(neg_pos_ratio_train))
-    """
-
-    #label_imputer = SimpleImputer(strategy="most_frequent")
-    #y_train = label_imputer.fit_transform(y_train)
-
-    # save the parameter bounds and a list of parameters that must be integers
-    params = {"max_depth": (6, 8),
-              "learning_rate": (0.001, 0.3),
-              "gamma": (0, 5)}
-    int_params = ["max_depth"]
-
-    def ba(y_true, y_pred):
-        conf_matrix = confusion_matrix(y_true, y_pred)
-        return single_balanced_accuracy_score(conf_matrix)
-
-    clf.tune_hyperparams(X=X_train,
-                         y=y_train,
-                         bounds=params,
-                         metric=ba,
-                         init_points=10,
-                         n_iter=25,
-                         int_params=int_params)
-
-    dump(clf.get_params(), "params_mixed.joblib")
-    param_dict = load("params_mixed.joblib")
-    print(param_dict)
-
-    preprocess = time.time()
-    print("Preprocess: {}".format(preprocess - start))
-
-    clf.fit(X_train, y_train, ignore_nan=True)
-
-    fit_time = time.time()
-    print("Fit time: {}".format(fit_time - preprocess))
-
-    y_pred = clf.predict(X_test)
-    # y_pred_bias = clf.predict(X_train)
-
-    pred_time = time.time()
-    print("Prediction time: {}".format(pred_time - fit_time))
-    print("Balanced accuracy: ", balanced_accuracy_score(y_test.T,
-                                                         y_pred,
-                                                         average="macro",
-                                                         zero_default=0))
-    """
-    print("Balanced accuracy bias:", balanced_accuracy_score(y_train.T,
-                                                             y_pred_bias,
-                                                             average="macro",
-                                                             zero_default=0))
-    """
-    print("---")
-    score_time = time.time()
-    print("Score time: {}".format(score_time - pred_time))
